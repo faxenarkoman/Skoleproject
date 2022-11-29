9fd961c278b5c622601009f923f63addc1caf873
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2022-11-29 14:12:36 +0100

    fck mig

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class Main {
-           static int input = 0;
+    static int input = 0;
+    static Scanner scan;
 
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        scan = new Scanner(System.in);
 
-        boolean run = false;
-        while(run = true)
-        {
+        boolean goback = true;
+        while (goback) {
             welcome();
             if (input == 1) {
                 System.out.println("Welcome Chairman, how do you want to proceed?");
@@ -15,13 +15,62 @@ public class Main {
                 System.out.println("2: Delete current member");
                 System.out.println("3: See all members");
                 System.out.println("4: Back to menu");
-                input = 0;
                 input = scan.nextInt();
-                if (input == 1) {
 
+                if (input == 1) {
+                    input = 0;
 
                 }
+
                 if (input == 2) {
+                    input = 0;
+
+                }
+
+                if (input == 3) {
+                    input = 0;
+
+                }
+
+                if (input == 4) {
+                    input = 0;
+                    goback = false;
+                }
+
+
+            }
+            while (goback) {
+                if (input == 2) {
+                    System.out.println("Welcome Trainer, how do you want to Proceed");
+                    System.out.println("1: Calendar");
+                    System.out.println("2: Log swimmers best time");
+                    System.out.println("3: Top 5 list");
+                    System.out.println("4: Back to menu");
+                    input = scan.nextInt();
+                    if (input == 1) {
+                        input = 0;
+
+                    }
+
+                    if (input == 2) {
+                        input = 0;
+
+                    }
+
+                    if (input == 3) {
+                        input = 0;
+
+                    }
+
+                    if (input == 4) {
+                        input = 0;
+                        goback = false;
+                    }
+
+                    if (input > 4) {
+                        input = 0;
+                        System.out.println("error");
+                    }
 
 
                 }
@@ -29,32 +78,31 @@ public class Main {
 
 
                 }
-
                 if (input == 4) {
-                    run = false;
-                    break;
+
+
                 }
+
 
                 if (input == 5) {
                     System.out.println("Have nice day!");
                     System.out.println("Goodbye");
                     System.exit(0);
                 }
+
             }
+
+        }
+    }
+        private static void welcome()
+        {
+            System.out.println("Welcome to Delfinen Choose your account");
+            System.out.println("(1. ---Chairman---)");
+            System.out.println("(2. ---Trainer---)");
+            System.out.println("(3. ---Cashier---)");
+            System.out.println("(4. ---Admin---)");
+            System.out.println("(5. ---Close Program---)");
+            input = scan.nextInt();
         }
 
-    }
-
-    private static void welcome()
-    {
-         System.out.println("Welcome to Delfinen Choose your account");
-         System.out.println("(1. ---Chairman---)");                    
-         System.out.println("(2. ---Trainer---)");                     
-         System.out.println("(3. ---Cashier---)");                     
-         System.out.println("(4. ---Admin---)");
-         System.out.println("(5. ---Close Program---)");
-         Scanner scan = new Scanner(System.in);
-         input = 0;
-         input = scan.nextInt();
-    }
 }
